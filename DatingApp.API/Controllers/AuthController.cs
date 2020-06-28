9a24c238f351622cc916515eef6ac2807afc02c2
paulkane22 <paulkane2204@hotmail.com>
2020-06-28 07:51:50 +0100

    End of Section 5 a

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -49,6 +49,9 @@ namespace DatingApp.API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
+            //throw new Exception("Computer says Error... PAUL KANE");
+
+
             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
             if (userFromRepo == null)
